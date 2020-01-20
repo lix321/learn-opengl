93a375ed4c9f6d74ae300eb86118d2cd77f7a5d5
lix-cd <44797085+lix-cd@users.noreply.github.com>
2020-01-20 17:13:56 +0800

    着色器

---
+++ shader.cpp
@@ -0,0 +1,36 @@
+// 着色器
+//输入变量也叫顶点属性，获取顶点属性的上限
+
+#include "iostream"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	// 初始化glfw
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	// 加载OpenGL函数指针
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	// 获取最大顶点属性个数
+	int nrAttributes;
+	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+}
